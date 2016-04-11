0ac8a413fa285b059e6aee9e2f8b19115e7e7aa9
tnvu1307 <tnvu1307@gmail.com>
2016-04-11 13:34:19 +0700

    a

--- ShopRuou/Controllers/ProductController.cs
+++ ShopRuou/Controllers/ProductController.cs
@@ -14,6 +14,7 @@ namespace ShopRuou.Controllers
             return View();
         }
 
+
         public ActionResult partialnhaSX()
         {
             using (var ctx = new QLShopRuouEntities1())
